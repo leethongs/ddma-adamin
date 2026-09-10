d23e6f19cd4a4b985155ea279f31f4003b4e9ee9
leethongs <sangtamofficial@gmail.com>
2026-09-10 11:22:10 +0530

    feat: actually add video support

--- lib/types.ts
+++ lib/types.ts
@@ -14,6 +14,7 @@ export interface Incident {
   damage_type: string;
   damage_details?: string;
   photo_url?: string;
+  video_url?: string;
   latitude?: number;
   longitude?: number;
   location_address?: string;
